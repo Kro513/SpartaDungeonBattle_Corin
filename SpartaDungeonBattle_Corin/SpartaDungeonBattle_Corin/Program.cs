1ccc9ff337d6ad54dfa19ca2f10862d0910bc392
[Github <141483527+Kro513@users.noreply.github.com>
2023-08-28 14:40:29 +0900

    Update Program.cs

--- SpartaDungeonBattle_Corin/SpartaDungeonBattle_Corin/Program.cs
+++ SpartaDungeonBattle_Corin/SpartaDungeonBattle_Corin/Program.cs
@@ -1,12 +1,53 @@
 ﻿namespace SpartaDungeonBattle_Corin
 {
-	internal class Program
+	static class EndBattle
 	{
-		int Damage;
+		static bool BattleResult = true;
 
-		static void Main(string[] args)
+		static void DisplayBattleResult()
 		{
-			Console.WriteLine("전투준비");
+			Console.Clear();
+
+			DisplayTitle("Battle!! - Result")
+
+			Console.WriteLine();
+			Console.WriteLine($"{BattleResult}");
+			Console.WriteLine();
+			
+			if (BattleResult = true)
+			{
+				Console.WriteLine($"던전에서 몬스터 {HowManyEnemy}마리를 잡았습니다.");
+				Console.WriteLine();
+				Console.WriteLine($"Lv. {player.Level} {player.Name}");
+				Console.WriteLine($"Hp {player.Hp} -> {player.Hp}");
+			}
+			else
+			{
+				Console.WirteLine("You Lose");
+				Console.WriteLine();
+				Console.WriteLine($"Lv. {player.Level} {player.Name}");
+				Console.WriteLine($"Hp {player.Hp} -> 0");
+			}
+			
+			Console.WriteLine();
+			Console.WriteLine("0. 다음");
+
+			int input = CheckValidInput(0, 0);
+			switch (input)
+			{
+				case 0:
+					DisplayEndGame();
+					break;
+			}
+		}
+
+		static void DisplayEndGame()
+		{
+			Console.BackgroundColor = ConsoleColor.White;
+			Console.ForegroundColor = ConsoleColor.Magenta;
+			Console.WriteLine("===Game Over===")
 		}
 	}
+
+	
 }
