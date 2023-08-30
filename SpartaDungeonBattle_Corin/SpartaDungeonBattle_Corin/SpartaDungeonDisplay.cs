ef3c9a502f0a160f1876e1de82c7df71d5e5977c
koju2001 <koju200555@gmail.com>
2023-08-30 17:45:55 +0900

    bug,코드리팩토링

--- SpartaDungeonBattle_Corin/SpartaDungeonBattle_Corin/SpartaDungeonDisplay.cs
+++ SpartaDungeonBattle_Corin/SpartaDungeonBattle_Corin/SpartaDungeonDisplay.cs
@@ -10,186 +10,209 @@ using static SpartaDungeonBattle_Corin.Program;
 
 namespace SpartaDungeonBattle_Corin
 {
-	public class Program
-	{
-		public static Character player;
 
-		public delegate void TakedamageHandler(int damage, string name);
+    public class Program
+    {
+        public static Character player = new Character("", "", 0, 0, 0, 0, 0, false);
 
-		static void Main(string[] args)
-		{
-			CharacterSetting();
-            GameStartScreen();
+        public delegate void TakedamageHandler(int damage, string name);
+
+
+        static void Main(string[] args)
+        {
             DisplayStartScreen();
-			DisplayGameIntro();
-			DisplayStatus();
+            DisplayGameIntro();
 
-		}
-
-		#region 초기설정
-		public static void CharacterSetting()
-		{
-			//캐릭터 정보 세팅
-			player = new Character("플레이어", "전사", 1, 100, 100, 10, 10, 1500, false);
-		}
-
-		#endregion
-
-		#region 게임 화면 출력
-
-		public static void GameStartScreen()
-		{
-			Console.Title = "스파르타던전";
-
-			Console.WriteLine("1. 게임 시작");
-			Console.WriteLine("2. 게임 종료");
-
-			Console.Write("\n원하시는행동을 입력해주세요: ");
-			Console.Write(">>");
+        }
 
 
-			int choice = int.Parse(Console.ReadLine());
+        #region 게임 화면 출력
 
-			switch (choice)
-			{
-				case 1:
-					Console.WriteLine("\n게임을 시작합니다!");
+        //첫 화면
+        static void DisplayStartScreen()
+        {
+            Console.Clear();
 
+            Console.WriteLine();
+            Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+            Console.WriteLine();
+            Console.WriteLine("먼저 캐릭터를 생성해주세요!.\n");
+            Console.WriteLine();
 
+            Console.WriteLine("0. 캐릭터 생성");
 
-					break;
+            int input = CheckValidInput(0, 0);
+            switch (input)
+            {
+                case 0:
+                    DisplayMakeCharacter();
+                    break;
+            }
 
+        }
 
-				case 2:
-					Console.WriteLine("\n게임을 종료합니다");
-					Environment.Exit(0);
+        static void DisplayMakeCharacter()
+        {
+            Console.Clear();
 
+            Console.WriteLine();
+            DisplayTitle("캐릭터 생성");
+            Console.WriteLine();
+            Console.Write("닉네임을 입력해주세요: ");
+            string playerName = Console.ReadLine();
+            player.Name = playerName;
 
-					break;
+            Console.Clear();
 
+            Console.WriteLine();
+            Console.WriteLine($"환영합니다! {playerName}님!");
+            Console.WriteLine();
+            Console.WriteLine("직업을 선택해주세요:");
+            Console.WriteLine();
+            Console.WriteLine("1. 전사");
+            Console.WriteLine();
+            Console.WriteLine("2. 마법사");
+            Console.WriteLine();
+            Console.WriteLine("3. 궁수");
 
-				default:
-					Console.WriteLine("\n잘못된 입력입니다.");
+            int input = CheckValidInput(1, 3);
 
+            Random random = new Random();
+            switch (input)
+            {
+                case 1:
+                    player.Class = "전사";
+                    player.Atk = random.Next(1, 11);
+                    player.Def = random.Next(1, 11);
+                    player.Hp = random.Next(70, 110);
+                    player = new Character(player.Name, player.Class, 1, player.Hp, player.Atk, player.Def, 1500, false);
 
-					break;
-			}
-		}
+                    break;
+                case 2:
+                    player.Class = "마법사";
+                    player.Atk = random.Next(1, 11);
+                    player.Def = random.Next(6, 11);
+                    player.Hp = random.Next(20, 110);
+                    player = new Character(player.Name, player.Class, 1, player.Hp, player.Atk, player.Def, 0, false);
+                    break;
+                case 3:
+                    player.Class = "궁수";
+                    player.Atk = random.Next(6, 11);
+                    player.Def = random.Next(1, 11);
+                    player.Hp = random.Next(20, 110);
+                    player = new Character(player.Name, player.Class, 1, player.Hp, player.Atk, player.Def, 0, false);
+                    break;
+            }
 
-            //첫 화면
-            static void DisplayStartScreen()
-		{
-			Console.Clear();
-
-			Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
-			Console.WriteLine("이제 전투를 시작할 수 있습니다.\n");
-			Console.WriteLine();
-
-		}
-
-		//게임 진입
-		static void DisplayGameIntro()
-		{
-			Battle battle = new Battle();
-
-			Console.Clear();
-
-			Console.WriteLine("1. 상태 보기");
-			Console.WriteLine("2. 전투 시작");
-			Console.WriteLine();
-			Console.WriteLine("원하시는 행동을 입력해주세요: ");
-			Console.WriteLine(">>");
-
-			int input = CheckValidInput(1, 2);
-
-			switch (input)
-			{
-				case 1:
-					Console.WriteLine("상태를 확인합니다."); // 상태 보기에 관한 내용을 추가할 수 있습니다.
-					DisplayStatus();
-					break;
-
-				case 2:
-					Console.WriteLine("전투를 시작합니다."); // 전투 시작에 관한 내용을 추가할 수 있습니다.
-					battle.BattleStart();
-					break;
-			}
-		}
-
-		//상태창
-		static void DisplayStatus()
-		{
-			Console.Clear();
-
-			DisplayTitle("상태 보기");
-			Console.WriteLine("캐릭터의 정보가 표시됩니다.");
-			Console.WriteLine();
-			Console.WriteLine($"Lv. {player.Level}");
-			Console.WriteLine($"{player.Name}({player.Class})");
-			Console.WriteLine($"공격력 : {player.Atk}");
-			Console.WriteLine($"방어력 : {player.Def}");
-			Console.WriteLine($"체력 : {player.Hp}");
-			Console.WriteLine($"Gold : {player.Gold} G\n");
-
-			Console.WriteLine();
-			Console.WriteLine("0. 나가기");
-			Console.WriteLine();
-			Console.WriteLine("원하시는 행동을 입력해주세요");
-			Console.WriteLine(">>");
+        }
 
 
-			int input = CheckValidInput(0, 0);
-			switch (input)
-			{
-				case 0:
-					Console.WriteLine("프로그램을 종료합니다.");
-					DisplayGameIntro();
-					break;
-			}
-		}
 
-		#endregion
+        //게임 진입
+        static void DisplayGameIntro()
+        {
+            Battle battle = new Battle();
 
-		#region Utility
+            Console.Clear();
 
-		//화면 전환 함수
-		static int CheckValidInput(int min, int max)
-		{
-			while (true)
-			{
-				string input = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("1. 상태 보기");
+            Console.WriteLine();
+            Console.WriteLine("2. 전투 시작");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요: ");
+            Console.WriteLine(">>");
 
-				bool parseSuccess = int.TryParse(input, out var ret);
-				if (parseSuccess)
-				{
-					if (ret >= min && ret <= max)
-						return ret;
-				}
+            int input = CheckValidInput(1, 2);
 
-				DisplayError("잘못된 입력입니다.");
-			}
-		}
+            switch (input)
+            {
+                case 1:
+                    Console.WriteLine("상태를 확인합니다."); // 상태 보기에 관한 내용을 추가할 수 있습니다.
+                    DisplayStatus();
+                    break;
 
-		//화면이름 색상설정
-		static void DisplayTitle(string title)
-		{
-			Console.ForegroundColor = ConsoleColor.Yellow;
-			Console.WriteLine(title);
-			Console.ResetColor();
-		}
+                case 2:
+                    Console.WriteLine("전투를 시작합니다."); // 전투 시작에 관한 내용을 추가할 수 있습니다.
+                    battle.BattleStart();
+                    break;
+            }
+        }
 
-		//에러
-		static void DisplayError(string error)
-		{
-			Console.ForegroundColor = ConsoleColor.Red;
-			Console.WriteLine(error);
-			Console.ResetColor();
-		}
+        //상태창
+        static void DisplayStatus()
+        {
+            Console.Clear();
 
-		//색상 바꿀 때 숫자로
-		public static void ChangeColor(string message, int consoleColor)
-		{
-			/* 
+            DisplayTitle("상태 보기");
+            Console.WriteLine("캐릭터의 정보가 표시됩니다.");
+            Console.WriteLine();
+            Console.WriteLine($"Lv. {player.Level}");
+            Console.WriteLine($"{player.Name}({player.Class})");
+            Console.WriteLine($"공격력 : {player.Atk}");
+            Console.WriteLine($"방어력 : {player.Def}");
+            Console.WriteLine($"체력 : {player.Hp}");
+            Console.WriteLine($"Gold : {player.Gold} G\n");
+
+            Console.WriteLine();
+            Console.WriteLine("0. 나가기");
+            Console.WriteLine();
+            Console.WriteLine("원하시는 행동을 입력해주세요");
+            Console.WriteLine(">>");
+
+
+            int input1 = CheckValidInput(0, 0);
+            switch (input1)
+            {
+                case 0:
+                    Console.WriteLine("프로그램을 종료합니다.");
+                    DisplayGameIntro();
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Utility
+
+        //화면 전환 함수
+        static int CheckValidInput(int min, int max)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                bool parseSuccess = int.TryParse(input, out var ret);
+                if (parseSuccess)
+                {
+                    if (ret >= min && ret <= max)
+                        return ret;
+                }
+
+                DisplayError("잘못된 입력입니다.");
+            }
+        }
+
+        //화면이름 색상설정
+        static void DisplayTitle(string title)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(title);
+            Console.ResetColor();
+        }
+
+        //에러
+        static void DisplayError(string error)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(error);
+            Console.ResetColor();
+        }
+
+        //색상 바꿀 때 숫자로
+        public static void ChangeColor(string message, int consoleColor)
+        {
+            /* 
 			 0 검정
 			 1 진파랑
 			 2 진녹색
@@ -204,10 +227,10 @@ namespace SpartaDungeonBattle_Corin
 			 11~~~~
 			 */
 
-			Console.ForegroundColor = (ConsoleColor)consoleColor;
-			Console.WriteLine(message, consoleColor);
-			Console.ResetColor();
-		}
-		#endregion
-	}
+            Console.ForegroundColor = (ConsoleColor)consoleColor;
+            Console.WriteLine(message, consoleColor);
+            Console.ResetColor();
+        }
+        #endregion
+    }
 }
